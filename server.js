1122a07c7b9e04badc242c65ba905817c7e90db6
Walter Wisner <your-email@example.com>
2026-07-12 21:18:48 -0400

    Fix CSP blocking image thumbnails

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(helmet({
       scriptSrc: ["'self'", "'unsafe-inline'"],
       scriptSrcAttr: ["'unsafe-inline'"],
       styleSrc: ["'self'", "'unsafe-inline'"],
-      imgSrc: ["'self'", 'data:'],
+      imgSrc: ["'self'", 'data:', 'blob:'],
       connectSrc: ["'self'"],
     },
   },
